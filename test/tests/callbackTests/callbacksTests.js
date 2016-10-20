19cffefa927a9d239764214bac53812059f8bd33
Bharath Sekar <bharath.sekar@ge.com>
2016-10-19 17:39:57 -0700

    Add test case for logout scenario [#132631681] https://www.pivotaltracker.com/story/show/132631681
    Signed-off-by: Jeremy Coffield <jcoffield@pivotal.io>

--- test/tests/callbackTests/callbacksTests.js
+++ test/tests/callbackTests/callbacksTests.js
@@ -1,6 +1,6 @@
 const loginUrl = 'http://localhost:8080/uaa/login';
-var testAppUrl = 'http://localhost:8000/test/tests/callbackTests/testpage.html';
-
+const testAppUrl = 'http://localhost:8000/test/tests/callbackTests/testpage.html';
+const logoutUrl = 'http://localhost:8080/uaa/logout.do';
 module.exports = {
   'test logout callback is called on page load without login' : function (browser) {
     browser
@@ -61,6 +61,36 @@ module.exports = {
         this.assert.equal(result.value.logouts, 1);
         this.assert.equal(result.value.identitiesReceived.length, 1);
         this.assert.equal(result.value.identitiesReceived[0].user_name, 'marissa')
+      })
+      .end();
+  },
+
+  'test identity change and logout callback is called when logged out' : function (browser) {
+    browser
+      .url(testAppUrl)
+      .waitForElementVisible('body', 1000)
+      .execute(function(url, name){
+        window.open(url, name);
+      }, [loginUrl, 'LOGIN_WINDOW'])
+      .waitForElementVisible('body', 1000)
+      .switchWindow('LOGIN_WINDOW', function(result){
+        console.log("Switch to login_window returned: ", result);
+      })
+      .setValue('input[name="username"]', 'marissa')
+      .setValue('input[name="password"]', 'koala')
+      .submitForm('input[name="username"]')
+      .pause(1200)
+      .url(logoutUrl)
+      .closeWindow()
+      .windowHandles(function(result) {
+        browser.switchWindow(result.value[0], function(result) { console.log('Switch window returned: ', result); });
+      })
+      .pause(1200)
+      .execute(function() {return info;}, [], function (result) {
+        console.log(result);
+        this.assert.equal(result.status, 0);
+        this.assert.equal(result.value.logouts, 2);
+        this.assert.equal(result.value.identitiesReceived.length, 1);
       })
       .end();
   }
