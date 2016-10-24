588911c8c964a24849f245e0cd0b68c932c066ac
Bharath Sekar <bharath.sekar@ge.com>
2016-10-24 10:09:19 -0700

    Fix for test cases to run on travis. Provide install scripts in travis yml

--- test/startHttpServer.sh
+++ test/startHttpServer.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 ./node_modules/http-server/bin/http-server > test/tmp/httpServer.log 2> test/tmp/httpServer.err.log -p 8000 &
 echo $! > test/tmp/httpServer.pid.tmp
--- test/tests/callbackTests/callbacksTests.js
+++ test/tests/callbackTests/callbacksTests.js
@@ -15,81 +15,81 @@ module.exports = {
       })
       .end();
   },
-  //
-  // 'test identity change callback is called on page load while logged in' : function (browser) {
-  //   browser
-  //     .url(loginUrl)
-  //     .waitForElementVisible('body', 1000)
-  //     .setValue('input[name="username"]', 'marissa')
-  //     .setValue('input[name="password"]', 'koala')
-  //     .submitForm('input[name="username"]')
-  //     .pause(1200)
-  //     .url(testAppUrl)
-  //     .pause(1200)
-  //     .execute(function() {return info;}, [], function (result) {
-  //       this.assert.equal(result.status, 0);
-  //       this.assert.equal(result.value.logouts, 0);
-  //       this.assert.equal(result.value.identitiesReceived.length, 1);
-  //       this.assert.equal(result.value.identitiesReceived[0].user_name, 'marissa')
-  //     })
-  //     .end();
-  // },
-  //
-  // 'test identity change callback is called when logged in' : function (browser) {
-  //   browser
-  //     .url(testAppUrl)
-  //     .waitForElementVisible('body', 1000)
-  //     .pause(1200)
-  //     .execute(function() {return info;}, [], function (result) {
-  //       this.assert.equal(result.status, 0);
-  //       this.assert.equal(result.value.logouts, 1);
-  //       this.assert.equal(result.value.identitiesReceived.length, 0);
-  //     })
-  //     .click('#loginWindow')
-  //     .switchWindow('LOGIN_WINDOW', function(result) { console.log('Switch window returned: ', result); })
-  //     .waitForElementVisible('body', 1000)
-  //     .setValue('input[name="username"]', 'marissa')
-  //     .setValue('input[name="password"]', 'koala')
-  //     .submitForm('input[name="username"]')
-  //     // .closeWindow() //This clears all session information including the storedInformation
-  //     .windowHandles(function(result) {
-  //       browser.switchWindow(result.value[0], function(result) { console.log('Switch window returned: ', result); });
-  //     })
-  //     .pause(1200)
-  //     .execute(function() {return info;}, [], function (result) {
-  //       this.assert.equal(result.status, 0);
-  //       this.assert.equal(result.value.logouts, 1);
-  //       this.assert.equal(result.value.identitiesReceived.length, 1);
-  //       this.assert.equal(result.value.identitiesReceived[0].user_name, 'marissa')
-  //     })
-  //     .end();
-  // },
-  //
-  // 'test identity change and logout callback is called when logged out' : function (browser) {
-  //   browser
-  //     .url(testAppUrl)
-  //     .waitForElementVisible('body', 1000)
-  //     .pause(1200)
-  //     .click('#loginWindow')
-  //     .waitForElementVisible('body', 1000)
-  //     .switchWindow('LOGIN_WINDOW', function(result){
-  //       console.log("Switch to login_window returned: ", result);
-  //     })
-  //     .setValue('input[name="username"]', 'marissa')
-  //     .setValue('input[name="password"]', 'koala')
-  //     .submitForm('input[name="username"]')
-  //     .pause(1200)
-  //     .url(logoutUrl)
-  //     .windowHandles(function(result) {
-  //       browser.switchWindow(result.value[0], function(result) { console.log('Switch window returned: ', result); });
-  //     })
-  //     .pause(1200)
-  //     .execute(function() {return info;}, [], function (result) {
-  //       console.log(result);
-  //       this.assert.equal(result.status, 0);
-  //       this.assert.equal(result.value.logouts, 2);
-  //       this.assert.equal(result.value.identitiesReceived.length, 1);
-  //     })
-  //     .end();
-  // }
+
+  'test identity change callback is called on page load while logged in' : function (browser) {
+    browser
+      .url(loginUrl)
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name="username"]', 'marissa')
+      .setValue('input[name="password"]', 'koala')
+      .submitForm('input[name="username"]')
+      .pause(1200)
+      .url(testAppUrl)
+      .pause(1200)
+      .execute(function() {return info;}, [], function (result) {
+        this.assert.equal(result.status, 0);
+        this.assert.equal(result.value.logouts, 0);
+        this.assert.equal(result.value.identitiesReceived.length, 1);
+        this.assert.equal(result.value.identitiesReceived[0].user_name, 'marissa')
+      })
+      .end();
+  },
+
+  'test identity change callback is called when logged in' : function (browser) {
+    browser
+      .url(testAppUrl)
+      .waitForElementVisible('body', 1000)
+      .pause(1200)
+      .execute(function() {return info;}, [], function (result) {
+        this.assert.equal(result.status, 0);
+        this.assert.equal(result.value.logouts, 1);
+        this.assert.equal(result.value.identitiesReceived.length, 0);
+      })
+      .click('#loginWindow')
+      .switchWindow('LOGIN_WINDOW', function(result) { console.log('Switch window returned: ', result); })
+      .waitForElementVisible('body', 1000)
+      .setValue('input[name="username"]', 'marissa')
+      .setValue('input[name="password"]', 'koala')
+      .submitForm('input[name="username"]')
+      // .closeWindow() //This clears all session information including the storedInformation
+      .windowHandles(function(result) {
+        browser.switchWindow(result.value[0], function(result) { console.log('Switch window returned: ', result); });
+      })
+      .pause(1200)
+      .execute(function() {return info;}, [], function (result) {
+        this.assert.equal(result.status, 0);
+        this.assert.equal(result.value.logouts, 1);
+        this.assert.equal(result.value.identitiesReceived.length, 1);
+        this.assert.equal(result.value.identitiesReceived[0].user_name, 'marissa')
+      })
+      .end();
+  },
+
+  'test identity change and logout callback is called when logged out' : function (browser) {
+    browser
+      .url(testAppUrl)
+      .waitForElementVisible('body', 1000)
+      .pause(1200)
+      .click('#loginWindow')
+      .waitForElementVisible('body', 1000)
+      .switchWindow('LOGIN_WINDOW', function(result){
+        console.log("Switch to login_window returned: ", result);
+      })
+      .setValue('input[name="username"]', 'marissa')
+      .setValue('input[name="password"]', 'koala')
+      .submitForm('input[name="username"]')
+      .pause(1200)
+      .url(logoutUrl)
+      .windowHandles(function(result) {
+        browser.switchWindow(result.value[0], function(result) { console.log('Switch window returned: ', result); });
+      })
+      .pause(1200)
+      .execute(function() {return info;}, [], function (result) {
+        console.log(result);
+        this.assert.equal(result.status, 0);
+        this.assert.equal(result.value.logouts, 2);
+        this.assert.equal(result.value.identitiesReceived.length, 1);
+      })
+      .end();
+  }
 };
