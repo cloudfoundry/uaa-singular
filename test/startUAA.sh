4f9f7642e06cb6781912ed2aa8415e8ba2de5aee
Jennifer Hamon <jhamon@pivotal.io>
2018-01-05 11:17:00 -0800

    Update test dependencies
    - Bump nightwatch
    - Update Selenium
    - Change to chromedriver
    
    [#153568058] https://www.pivotaltracker.com/story/show/153568058
    
    Signed-off-by: Mikhail Vyshegorodtsev <medvedzver@inbox.ru>

--- test/startUAA.sh
+++ test/startUAA.sh
@@ -13,3 +13,24 @@ while [ true ]; do
   fi
   sleep 5;
 done
+
+ACCESS_TOKEN=$(curl http://localhost:8080/uaa/oauth/token -X POST  \
+    -H 'Content-Type: application/x-www-form-urlencoded' \
+    -H 'Accept: application/json' \
+    -d 'client_id=admin&client_secret=adminsecret&grant_type=client_credentials&token_format=opaque&response_type=token' \
+    | jq '.access_token' \
+    | tr -d '"')
+
+curl http://localhost:8080/uaa/oauth/clients -X POST \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer ${ACCESS_TOKEN}" \
+    -H 'Accept: application/json' \
+    -d '{
+  "scope" : [ "cloud_controller.read", "cloud_controller.write", "openid" ],
+  "client_id" : "singular-test-client",
+  "resource_ids" : [ ],
+  "authorized_grant_types" : [ "implicit" ],
+  "redirect_uri" : [ "http://localhost:8000/**","http://localhost:8080/**" ],
+  "autoapprove" : true,
+  "name" : "Singular Test Client"
+}'
