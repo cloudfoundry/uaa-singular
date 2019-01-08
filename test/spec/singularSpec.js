b0a2d5d4eb212cdc45ac8d434cf92ac4907481d9
Aakash Shah <ashah@pivotal.io>
2019-01-08 14:40:06 -0800

    Backfill tests for DOMContentLoaded
    Signed-off-by: Aakash Shah <ashah@pivotal.io>

--- test/spec/singularSpec.js
+++ test/spec/singularSpec.js
@@ -1,11 +1,15 @@
 var document = {
   getElementById: function() { return { src: "" }},
   createElement: function() { return { setAttribute: function() {} }},
-  addEventListener: function() {}
+  addEventListener: function() {},
+  body: {
+    appendChild: function() {}
+  }
 };
 var window = {
   atob: function() {},
-  location: { href: 'http://mytesturl' }
+  location: { href: 'http://mytesturl' },
+  parent: {}
 };
 
 describe("Singular", function () {
@@ -27,13 +31,46 @@ describe("Singular", function () {
       expect(badinit).toThrow("The \"uaaLocation\" field must be set and not empty");
     });
 
-    it("validates the uaaLocation is actually a UAA", function() {
-      var badinit = function() {
-        Singular.init({clientId: "boo", uaaLocation: "not-a-uaa-url", singularLocation: './node_modules/uaa-singular/singular' });
-      };
+    describe("when the uaaLocation is actually a UAA", function () {
+      var domContentLoaded;
 
-      badinit();
-      expect(Singular.getUaaValidator().isValidUAA).toHaveBeenCalled();
+      beforeEach(function () {
+        spyOn(document, 'addEventListener').and.callFake((_, cb) => domContentLoaded = cb);
+        spyOn(document.body, 'appendChild').and.callThrough();
+
+        Singular.init({clientId: "boo", uaaLocation: "not-a-uaa-url", singularLocation: './node_modules/uaa-singular/singular' });
+      });
+
+      it("validates the uaaLocation", function() {
+        expect(Singular.getUaaValidator().isValidUAA).toHaveBeenCalled();
+      });
+
+      it('adds a dom content loaded event listener', function () {
+        expect(document.addEventListener).toHaveBeenCalledWith("DOMContentLoaded", domContentLoaded);
+      });
+
+      it("does not assign Singular to parent window", function () {
+        expect(window.parent.Singular).not.toBeFalsy();
+      });
+
+      it("append Singular iframes", function () {
+        expect(document.body.appendChild).not.toHaveBeenCalled();
+      });
+
+      describe("when dom content has loaded", function () {
+        beforeEach(function () {
+          domContentLoaded();
+        });
+
+        it("assigns Singular to parent window", function () {
+          expect(window.parent.Singular).toBe(Singular);
+        });
+
+        it("append Singular iframes", function () {
+          expect(document.body.appendChild).toHaveBeenCalledWith(Singular.opFrame);
+          expect(document.body.appendChild).toHaveBeenCalledWith(Singular.rpFrame);
+        });
+      });
     });
 
     it("validates clientId is set", function() {
